77f8b48b7f30a573c25cbbee9d6003e10437ff97
felipegebara08 <144124431+felipegebara08@users.noreply.github.com>
2023-11-16 15:45:10 -0300

    Salvamento_03
    salvamento 3, quase terminando o projeto

--- back_end/app.js
+++ back_end/app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
 //criar uma pool de conexão
 const pool = mysql.createPool({
     host: 'localhost',
@@ -23,8 +24,9 @@ app.get('/api/midia', async (req,res) => {
     try {
     const conection = await pool.getConnection()
     const sql = "SELECT * FROM midia"
-    const [linhas] = await conexao.execute(sql)
-    conexao.release()
+    const [linhas] = await conection.execute(sql)
+    console.log(linhas)
+    conection.release()
     res.json(linhas)
 
     } catch(error) {
@@ -37,13 +39,13 @@ app.get('/api/midia', async (req,res) => {
 app.post("/api/midia/", async(req, res) => {
     try {
         const {nome, tipo, status, data_inicio, data_fim, tempo} = req.body
-        const conexao = await pool.getConnection()
+        const conection = await pool.getConnection()
         const sql = `INSERT INTO midia (nome, tipo, status, data_inicio, data_fim, tempo) 
                     VALUES ("${nome}", "${tipo}", "${status}", "${data_inicio}",
                      "${data_fim}", "${tempo}")`
         console.log(sql)
-        const [linhas] = await conexao.execute(sql)
-        conexao.release()
+        const [linhas] = await conection.execute(sql)
+        conection.release()
         res.json({msg: "Registro gravado!"})
         
     } catch (error) {
@@ -56,13 +58,13 @@ app.post("/api/midia/", async(req, res) => {
 app.put("/api/midia/", async(req,res) =>{
     try {
         const {id ,nome, tipo, status, data_inicio, data_fim, tempo} = req.body
-        const conexao = await pool.getConnection()
+        const conection = await pool.getConnection()
         const sql = `UPDATE midia SET nome = "${nome}", tipo = "${tipo}", status = "${status}",
                     data_inicio = "${data_inicio}", data_fim = "${data_fim}", tempo = "${tempo}}"
                     WHERE id = ${id}`
         console.log(sql)
-        const [linhas] = await conexao.execute(sql  )
-        conexao.release()
+        const [linhas] = await conection.execute(sql  )
+        conection.release()
         res.json({msg: "Registro gravado!"})
         
     } catch (error) {
@@ -75,11 +77,11 @@ app.put("/api/midia/", async(req,res) =>{
 app.delete("/api/midia/:id", async(req, res) => {
     try {
         const id_DELETE = req.params.id
-        const conexao = await pool.getConnection()
+        const conection = await pool.getConnection()
         const sql = `DELETE FROM midia WHERE id = ${id_DELETE}`
         console.log(sql)
-        const [linhas] = await conexao.execute(sql)
-        conexao.release()
+        const [linhas] = await conection.execute(sql)
+        conection.release()
         res.json({msg: "Registro excluido!"})
         
     } catch (error) {
--- front_end/script.js
+++ front_end/script.js
@@ -5,33 +5,46 @@ const btn_tela_atualizar = document.getElementById("btn_tela_atualizar")
 const btn_tela_excluir = document.getElementById("btn_tela_excluir")
 
 //mostrar e ocultar div
-function mostrar_div() {
-    document.getElementById("tela_cadastro").style.display = "flex"
+function mostrar_div(id_div) {
+    document.getElementById(id_div).style.display = "flex"
 }
-function ocultar_div() {
-    document.getElementById("tela_cadastro").style.display = "none"
+function ocultar_div(id_div) {
+    document.getElementById(id_div).style.display = "none"
 }
 
 // Botões de ação dentro de cada DIV
 const btn_select = document.getElementById("verifica");
 const btn_cadastro = document.getElementById("btn_cadastro")
-
 const btn_atualizar_dados = document.getElementById("btn_atualizar_dados")
 
 btn_tela_cadastrar.addEventListener("click", () => {
     mostrar_div("tela_cadastro")
+    ocultar_div("Tela_busca")
+    ocultar_div("Tela_atualizar")
+})
+
+btn_tela_busca.addEventListener("click", () => {
+    mostrar_div("Tela_busca")
+    ocultar_div("tela_cadastro")
+    ocultar_div("Tela_atualizar")
+})
+
+btn_tela_atualizar.addEventListener("click", () => {
+    mostrar_div("Tela_atualizar")
+    ocultar_div("tela_cadastro")
+    ocultar_div("Tela_busca")
 })
 
 btn_cadastro.addEventListener("click", async () => {
-    const nome = document.getElementById("nome_midia").value
+    const nome = document.getElementById("nome_da_midia").value
     const tipo = document.getElementById("tipo").value
     const status = document.getElementById("status").value
-    const data_inicio = document.getElementById("data_inicio").value
+    const data_inicio = document.getElementById("data_inicio").value 
     const data_fim = document.getElementById("data_fim").value
     const url = document.getElementById("url").value
     const tempo = document.getElementById("tempo").value
 
-    let dados = await fetch("http://localhost:3000/api/cadastro", {
+    let dados = await fetch("http://localhost:3000/api/midia", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -55,6 +68,10 @@ btn_select.addEventListener("click", async () => {
                     <th scope="col">id</th>
                     <th scope="col" class='text-start'>Nome</th>
                     <th scope="col" class='text-start'>tipo</th>
+                    <th scope="col" class='text-start'>status</th>
+                    <th scope="col" class='text-start'>tempo</th>
+                    <th scope="col" class='text-start'>data_inicial</th>
+                    <th scope="col" class='text-start'>data_final</th>
                     <th scope="col">Editar</i></th>
                     <th scope="col">Excluir</th>
                   </tr>
@@ -75,22 +92,33 @@ btn_select.addEventListener("click", async () => {
         resposta = await fetch(`http://localhost:3000/api/midia/tipo/${busca}`);
     } else if (opcao == "status") {
         resposta = await fetch(`http://localhost:3000/api/midia/status/${busca}`);
-    } else if (opcao == "data_inicio") {
-        resposta = await fetch(`http://localhost:3000/api/midia/data_inicio/${busca}`);
-    } else if (opcao == "data_fim") {
-        resposta = await fetch(`http://localhost:3000/api/midia/data_fim/${busca}`);
     }
 
     if (resposta.ok) {
         html = html;
         let array_resultado = await resposta.json();
-        if (opcao == "todos" || opcao == "nome" || opcao == "tipo") {
+        if (opcao == "todos" || opcao == "nome" || opcao == "tipo" || opcao == "status") {
             for (const dados of array_resultado) {
-                ddd
+                let dt_inicio = dados.data_inicio.substr(8,2)+"/"+dados.data_inicio.substr(5,2)+"/"+dados.data_inicio.substr(0,4)
+                let dt_fim = dados.data_fim.substr(8,2)+"/"+dados.data_fim.substr(5,2)+"/"+dados.data_fim.substr(0,4)
+
+                let tipo = ""
+                let status = ""
+
+                if(dados.tipo == "f") tipo = "Foto" 
+                else tipo = "video"
+
+                if(dados.status == "a") status = "ativo"
+                else status = "inativo"
+
                 html += `<tr>                
         <td>${dados.id}</td>
         <td class='text-start'>${dados.nome}</td>
-        <td class='text-start'>${dados.tipo}</td>
+        <td class='text-start'>${tipo}</td>
+        <td class='text-start'>${status}</td>
+        <td class='text-start'>${dados.tempo}</td>
+        <td class='text-start'>${dt_inicio}</td>
+        <td class='text-start'>${dt_fim}</td>
         <td><i onclick="editar(${dados.id})" class="bi bi-pencil"></td>
         <td><i onclick="excluir(${dados.id})" class="bi bi-trash"></i></td
         </tr>`;
@@ -149,15 +177,15 @@ btn_atualizar_dados.addEventListener("click", async () => {
     const status_atualizado = document.getElementById("status_editado").value
     const data_inicial_atualizado = document.getElementById("dataI_editado").value
     const data_final_atualizado = document.getElementById("dataF_editado").value
+    const url_atualizado = document.getElementById("url_editado").value
     const tempo_atualizado = document.getElementById("tempo_editado").value
-    const id = document.getElementById("id_editado").value
 
-    let dados = await fetch("http://localhost:3000/api/usuarios", {
+    let dados = await fetch("http://localhost:3000/api/midia", {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({ id: id, nome: nome_atualizado, tipo: tipo_atualizado, status: status_atualizado, dataI: data_inicial_atualizado, dataF: data_final_atualizado, tempo: tempo_atualizado, }),
+        body: JSON.stringify({ nome: nome_atualizado, tipo: tipo_atualizado, status: status_atualizado, dataI: data_inicial_atualizado, dataF: data_final_atualizado, ulr: url_atualizado, tempo: tempo_atualizado, }),
     });
 
     if (dados.ok) {
